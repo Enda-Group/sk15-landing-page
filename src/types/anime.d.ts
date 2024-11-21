1266b3da24542de0898febbd0d43f166dda722e9
ey3shuh <notaisha20@gmail.com>
2024-11-21 16:40:14 +0800

    change anime.d.ts

--- src/types/anime.d.ts
+++ src/types/anime.d.ts
@@ -1,5 +1,30 @@
 declare module "animejs" {
-    const anime: any;
+    interface AnimeInstance {
+      play: () => void;
+      pause: () => void;
+      restart: () => void;
+      reverse: () => void;
+      seek: (time: number) => void;
+      completed: boolean;
+    }
+  
+    interface AnimeParams {
+      targets: string | HTMLElement | NodeList | Array<string | HTMLElement>;
+      duration?: number;
+      delay?: number;
+      easing?: string;
+      loop?: boolean | number;
+      direction?: "normal" | "reverse" | "alternate";
+      autoplay?: boolean;
+      [key: string]: any; // Allows additional options
+    }
+  
+    interface Anime {
+      (params: AnimeParams): AnimeInstance;
+      timeline: (params?: AnimeParams) => AnimeInstance;
+    }
+  
+    const anime: Anime;
     export default anime;
   }
   
