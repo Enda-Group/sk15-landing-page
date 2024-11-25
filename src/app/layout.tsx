d61554c6ca2bfc2d90a24c0937c800a1a850a3a3
ey3shuh <notaisha20@gmail.com>
2024-11-25 11:47:09 +0800

    delete lines from layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,26 +1,8 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
 import Head from "next/head"; // Import the Head component
 import "./globals.css";
 
-// Importing your custom Poppins font
-const poppins = localFont({
-  src: "./fonts/Poppins-Black.woff", // Path to your Poppins font file
-  variable: "--font-poppins", // CSS variable to apply the font
-  weight: "900", // Specify the weight for Poppins-Black (900)
-});
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
 
 export const metadata: Metadata = {
   title: "Siti Khadijah",
